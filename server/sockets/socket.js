f496dd30cc13f61c1543320c6b71ba03a40249bc
israel <israel.bejarano.a@gmail.com>
2019-03-26 22:26:55 +0100

    on siguienteTicket

--- server/sockets/socket.js
+++ server/sockets/socket.js
@@ -1,36 +1,12 @@
 const { io } = require('../server');
 const { TicketControl } = require('../classes/ticket-control');
-const ticketControñ = new TicketControl();
+const ticketControl = new TicketControl();
 // on es escuchar eventos
 // emit es emitir informacion
 io.on('connection', (client) => {
-    console.log('cliente socket conectado');
-
-    client.emit('enviarMensaje', {
-        usuario: 'Admin',
-        mensaje: 'bienvenido a la aplicación'
-    });
-
-    client.on('disconnect', () => {
-        console.log('usuario desconectado');
-    });
-
-    client.on('enviarMensaje', (data, callback) => {
-        console.log(data);
-        /* client.broadcast.emit('enviarMensaje', {
-            usuario: data.usuario,
-            mensaje: data.mensaje
-        }); */
-        client.broadcast.emit('enviarMensaje', data); // es lo mismo que lo de arriba
-        /* if (mensaje.usuario) {
-            callback({
-                resp: 'TODO SALIO OK'
-            });
-        } else {
-            callback({
-                resp: 'TODO SALIO MAL'
-            });
-        } */
-
+    client.on('siguienteTicket', (data, callback) => {
+        let siguiente = ticketControl.siguiente();
+        console.log('siguiente ticket: ', siguiente);
+        callback(siguiente);
     });
 });
