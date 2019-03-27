65cb5bfafd6a211a77aa12c98c6b403bf0678909
israel <israel.bejarano.a@gmail.com>
2019-03-27 23:25:48 +0100

    utimos4 tickets

--- server/classes/ticket-control.js
+++ server/classes/ticket-control.js
@@ -1,13 +1,24 @@
 const fs = require('fs');
 
+class Ticket {
+    constructor(numero, escritorio) {
+        this.numero = numero;
+        this.escritorio = escritorio;
+    }
+}
+
 class TicketControl {
     constructor() {
         this.ultimo = 0;
         this.hoy = new Date().getDate();
+        this.tickets = [];
+        this.ultimos4 = [];
 
         let data = require('../data/data.json');
         if (data.hoy === this.hoy) {
             this.ultimo = data.ultimo;
+            this.tickets = data.tickets;
+            this.ultimos4 = data.ultimos4;
         } else {
             this.reiniciarConteo();
         }
@@ -15,12 +26,36 @@ class TicketControl {
 
     siguiente() {
         this.ultimo += 1;
+        let ticket = new Ticket(this.ultimo, null);
+        this.tickets.push(ticket);
         this.grabarArchivo();
         return `Ticket ${this.ultimo}`;
     }
 
+    getUltimoTicket() {
+        return `Ticket ${this.ultimo}`;
+    }
+
+    atenderTicket(escritorio) {
+        if (this.tickets.length === 0) {
+            return 'no hay tickets';
+        }
+        let numeroTicket = this.tickets[0].numero;
+        this.tickets.shift();
+        let atenderTicket = new Ticket(numeroTicket, escritorio);
+        this.ultimos4.unshift(atenderTicket);
+        if (this.ultimos4.length > 4) {
+            this.ultimos4.slice(-1, 1);
+        }
+        console.log('ultimos 4', this.ultimos4);
+        this.grabarArchivo();
+        return atenderTicket;
+    }
+
     reiniciarConteo() {
         this.ultimo = 0;
+        this.tickets = [];
+        this.ultimos4 = [];
         console.log('se ha inicializado el sistema');
         this.grabarArchivo();
     }
@@ -28,7 +63,9 @@ class TicketControl {
     grabarArchivo() {
         let jsonData = {
             ultimo: this.ultimo,
-            hoy: this.hoy
+            hoy: this.hoy,
+            tickets: this.tickets,
+            ultimos4: this.ultimos4
         };
         let jsonDataString = JSON.stringify(jsonData);
         fs.writeFileSync('./server/data/data.json', jsonDataString);
